83dffe13d0f6a9b8e16d57a2a27e1f1c857d8cc0
Vinicius C <viniciuscorreia54321@gmail.com>
2021-06-18 11:13:48 -0300

    crt

--- verificar.php
+++ verificar.php
@@ -4,30 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consulta</title>
+    <title>Verficar</title>
 </head>
 <body>
     <?php
         echo "<table style='border: solid 1px black;'>";
         echo "<tr><th>Id</th><th>Aluno</th><th>Disciplina</th><th>Nota I</th><th>Nota II</th></tr>";
-
         class TableRows extends RecursiveIteratorIterator {
         function __construct($it) {
             parent::__construct($it, self::LEAVES_ONLY);
-        }
+    }
 
         function current() {
             return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-        }
+    }
 
         function beginChildren() {
             echo "<tr>";
-        }
+    }
 
         function endChildren() {
             echo "</tr>" . "\n";
-        }
-        }
+    }
+}
 
         $servername = "bmlx3df4ma7r1yh4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         $username = "vkq7duqk43riyga7";
@@ -39,15 +38,13 @@
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $stmt = $conn->prepare("SELECT id, Aluno, Disciplina, Nota_I, Nota_II FROM dadosescola");
         $stmt->execute();
-
-        // set the resulting array to associative
         $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
         foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
             echo $v;
-        }
+    }
         } catch(PDOException $e) {
         echo "Error: " . $e->getMessage();
-        }
+    }
         $conn = null;
         echo "</table>";
     ?>
